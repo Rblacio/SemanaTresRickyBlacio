e63ca2274ced6ca79a663fc68c746a73d1f24c46
Ricky <Ricky@DESKTOP-4CJ7MM1>
2022-04-27 06:31:03 +0200

    Agregar archivos de proyecto.

---
+++ SemanaTresRickyBlacio/SemanaTresRickyBlacio/MainPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace SemanaTresRickyBlacio
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void btnAbrir_Clicked(object sender, EventArgs e)
+        {
+            string usuario = txtUsuario.Text;
+            string pass = txtPass.Text;
+
+            await Navigation.PushAsync(new page2(usuario,pass));
+        }
+    }
+}
---
+++ SemanaTresRickyBlacio/SemanaTresRickyBlacio/page2.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace SemanaTresRickyBlacio
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class page2 : ContentPage
+    {
+        public page2(string usuario, string contrasena) 
+        {
+            InitializeComponent();
+
+            // DisplayAlert("Datos enviados", "El usuario es "+ usuario + " La clave es " + contrasena, "cerrar");
+
+            lblNombre.Text = "El usuario es "+ usuario;
+            lblContrasena.Text = "La contrasena es " + contrasena;
+
+        
+    }
+    }
+}
